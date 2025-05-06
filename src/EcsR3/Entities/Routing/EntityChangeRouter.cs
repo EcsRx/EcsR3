b1451b584fd268b1d52ee171a1200b5d5f4f3fcb
LP <grofit1@yahoo.com>
2025-05-06 20:08:42 +0100

    Dont need dictionaries as we will have unique keys

--- src/EcsR3/Entities/Routing/EntityChangeRouter.cs
+++ src/EcsR3/Entities/Routing/EntityChangeRouter.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using EcsR3.Components.Lookups;
+﻿using EcsR3.Components.Lookups;
 using R3;
 using SystemsR3.Extensions;
 
@@ -7,9 +6,9 @@ namespace EcsR3.Entities.Routing
 {
     public class EntityChangeRouter : IEntityChangeRouter
     {
-        private readonly Dictionary<int, Subject<EntityChange>> _onEntityAddedComponent;
-        private readonly Dictionary<int, Subject<EntityChange>> _onEntityRemovingComponent;
-        private readonly Dictionary<int, Subject<EntityChange>> _onEntityComponentRemoved;
+        private readonly Subject<EntityChange>[] _onEntityAddedComponent;
+        private readonly Subject<EntityChange>[] _onEntityRemovingComponent;
+        private readonly Subject<EntityChange>[] _onEntityComponentRemoved;
         
         public IComponentTypeLookup ComponentTypeLookup { get; }
 
@@ -17,28 +16,23 @@ namespace EcsR3.Entities.Routing
         {
             ComponentTypeLookup = componentTypeLookup;
             
-            _onEntityAddedComponent = new Dictionary<int, Subject<EntityChange>>();
-            _onEntityRemovingComponent = new Dictionary<int, Subject<EntityChange>>();
-            _onEntityComponentRemoved = new Dictionary<int, Subject<EntityChange>>();
+            _onEntityAddedComponent = new Subject<EntityChange>[ComponentTypeLookup.AllComponentTypeIds.Length];
+            _onEntityRemovingComponent = new Subject<EntityChange>[ComponentTypeLookup.AllComponentTypeIds.Length];
+            _onEntityComponentRemoved = new Subject<EntityChange>[ComponentTypeLookup.AllComponentTypeIds.Length];
             
-            CreateComponentStreams();
-        }
-
-        public void CreateComponentStreams()
-        {
             foreach (var componentType in ComponentTypeLookup.AllComponentTypeIds)
             {
-                _onEntityAddedComponent.Add(componentType, new Subject<EntityChange>());
-                _onEntityRemovingComponent.Add(componentType, new Subject<EntityChange>());
-                _onEntityComponentRemoved.Add(componentType, new Subject<EntityChange>());
+                _onEntityAddedComponent[componentType] = new Subject<EntityChange>();
+                _onEntityRemovingComponent[componentType] = new Subject<EntityChange>();
+                _onEntityComponentRemoved[componentType] = new Subject<EntityChange>();
             }
         }
 
         public void Dispose()
         {
-            _onEntityAddedComponent.ForEachRun(x => x.Value.Dispose());
-            _onEntityRemovingComponent.ForEachRun(x => x.Value.Dispose());
-            _onEntityComponentRemoved.ForEachRun(x => x.Value.Dispose());
+            _onEntityAddedComponent.ForEachRun(x => x.Dispose());
+            _onEntityRemovingComponent.ForEachRun(x => x.Dispose());
+            _onEntityComponentRemoved.ForEachRun(x => x.Dispose());
         }
 
         public Observable<EntityChange> OnEntityAddedComponent(int componentType) => _onEntityAddedComponent[componentType];
